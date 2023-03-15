ebde18c8b784de01b9f76c392d39ef51f08bc277
feyza-unal <feyzaunal95@gmail.com>
2023-03-15 23:35:41 +0300

    api tests

---
+++ src/test/java/C1_GetRequestResponseBodyYazdirma.java
@@ -0,0 +1,35 @@
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class C1_GetRequestResponseBodyYazdirma {
+
+    @Test
+    public void get01() {
+
+        //https://restful-booker.herokuapp.com/booking/10 url'ine git
+        // bit GET req. gonderdigimizde donen res. yazdir
+
+        // 1- Request body ve end-point hazirlama
+        String url = "https://restful-booker.herokuapp.com/booking/10";
+
+        // 2- Expected data hazirlama
+
+        // 3- Request gonderip, donen response'u kaydetme
+        Response response = given().when().get(url);
+        response.prettyPrint(); // response'u yazdirma
+
+        // 4- Assertion
+
+    }
+}
+
+/* Response classinda obje olusturma
+    # Response response = given().when().get(url);
+    # given: teste baslarken bize verilen baslangic degerleri
+    # when: testte gerceklestirdigimiz islemler
+    # then: response degerlerini degerlendirmek icin yapilan islemler
+    # and: birbirine bagli islemler
+
+ */
