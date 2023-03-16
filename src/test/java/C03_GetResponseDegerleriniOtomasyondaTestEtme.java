f60087aac324943b82c3e923ba1410ce544b6852
feyza-unal <feyzaunal95@gmail.com>
2023-03-16 23:25:36 +0300

    api tests

---
+++ src/test/java/C03_GetResponseDegerleriniOtomasyondaTestEtme.java
@@ -0,0 +1,36 @@
+import io.restassured.response.Response;
+import org.junit.Test;
+
+import static io.restassured.RestAssured.given;
+
+public class C03_GetResponseDegerleriniOtomasyondaTestEtme {
+    @Test
+    public void test01() {
+        /*
+https://restful-booker.herokuapp.com/booking/10 url’ine bir
+GET request gonderdigimizde donen Response’un,
+status code’unun 200,
+ve content type’inin application/json; charset=utf-8,
+ve Server isimli Header’in degerinin Cowboy,
+ve status Line’in HTTP/1.1 200 OK oldugunu test edin */
+
+         //1- end point ve request body olustur
+        String url = "https://restful-booker.herokuapp.com/booking/10";
+
+        //2- Expected body olustur
+        //3- Request gonderip, donen response'i kaydet
+
+        Response response = given().when().get(url);
+
+        //Assertion
+        response
+                .then()
+                .assertThat()
+                .statusCode(200)
+                .contentType("application/json; charset=utf-8")
+                .header("Server","Cowboy")
+                .statusLine("HTTP/1.1 200 OK");
+
+
+    }
+}
