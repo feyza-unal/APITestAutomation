25ed0773536e24024dd558c139cbdb5860c503c6
feyza-unal <feyzaunal95@gmail.com>
2023-03-29 23:47:56 +0300

    api tests

--- src/test/java/baseUrl/BaseUrlDummyExample.java
+++ src/test/java/baseUrl/BaseUrlDummyExample.java
@@ -5,13 +5,11 @@ import io.restassured.specification.RequestSpecification;
 import org.junit.Before;
 
 public class BaseUrlDummyExample {
-
-    protected RequestSpecification specDummyExample;
-
+    protected RequestSpecification specDummyExample; //ilk once obje olusturduk
     @Before
     public void setup(){
 
-        specDummyExample= new RequestSpecBuilder()
+        specDummyExample = new RequestSpecBuilder()
                 .setBaseUri("http://dummy.restapiexample.com/api/v1")
                 .build();
     }
